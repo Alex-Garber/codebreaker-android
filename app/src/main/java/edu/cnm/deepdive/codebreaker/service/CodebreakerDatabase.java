cb7fbefd7e301dbcb1741afbdbee2182b0400b4e
Alex Garber <alexgarber6@gmail.com>
2020-10-14 11:54:34 -0600

    Data base implemented

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerDatabase.java
@@ -0,0 +1,54 @@
+package edu.cnm.deepdive.codebreaker.service;
+
+import android.app.Application;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+import edu.cnm.deepdive.codebreaker.model.dao.GameDao;
+import edu.cnm.deepdive.codebreaker.model.entity.Game;
+import edu.cnm.deepdive.codebreaker.service.CodebreakerDatabase.Converters;
+import java.util.Date;
+
+@Database(entities = {Game.class}, version = 1)
+@TypeConverters({Converters.class})
+public abstract class CodebreakerDatabase extends RoomDatabase {
+  // Name of file that is our data base
+  private static final String DB_NAME = "codebreaker";
+
+  // Must generate a setter for context
+  private static Application context;
+
+  public static void setContext(Application context) {
+    CodebreakerDatabase.context = context;
+  }
+  public static CodebreakerDatabase getInstance(){
+    return InstanceHolder.INSTANCE;
+
+  }
+  // creates a way to get instance of GameDao
+  public abstract GameDao getGameDao();
+// Builder builds what INSTANCE holds ie the database
+  private static class InstanceHolder {
+    private static final CodebreakerDatabase INSTANCE =
+        Room.databaseBuilder(context,CodebreakerDatabase.class, DB_NAME)
+        .build();
+  }
+// date obj to long
+  public static class Converters {
+    @TypeConverter
+    // date obj to long
+    public static Long dateToLong(Date value) {
+      return (value != null) ? value.getTime() : null;
+    }
+
+  @TypeConverter
+  // long to date
+  public static Date longToDate(Long value) {
+    return (value != null) ? new Date(value) : null;
+  }
+
+  }
+
+}
